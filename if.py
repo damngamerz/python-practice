6750511378e0d53699283c40ef97505028140024
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-18 10:40:10 +0530

    Jan 18

---
+++ if.py
@@ -0,0 +1,10 @@
+var1= 100
+if var1:
+        print("1 - Got a True Value")
+        print(var1)
+        
+var2=0
+if var2:
+        print ("2 - Got a True Value")
+        print(var2)
+print ("Good bye!")
---
+++ if_else.py
@@ -0,0 +1,15 @@
+var1=100
+if var1:
+        print("1.Condition is true")
+        print(var1)
+else:
+        print("1.Conditon is False")
+        print(var1)
+        
+var2=0
+if var2:
+        print("2.Condition is true")
+        print(var2)
+else:
+        print("2.Conditon is False")
+        print(var2)
