dbc1d9a2028f4fb82ed5d0373d67633be1eddb0f
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-19 23:52:27 +0530

    19 Jan

---
+++ string_features.py
@@ -0,0 +1,18 @@
+#using triple quotes
+para_str = """this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( \t ) and they will show up that way when displayed.
+NEWLINEs within the string, whether explicitly given like
+this within the brackets [ \n ], or just a NEWLINE within
+the variable assignment will also show up.
+"""
+print (para_str)
+
+#raw string
+
+print('C:\\nowhere')
+
+#using r'expression'
+
+print(r'C:\\nowhere')
+
