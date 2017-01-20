d2ff3de26955169b49ebb8909441e194864168bb
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-20 11:13:20 +0530

    20 Jan

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+#Creating Dictionary and accessing values
+
+dict ={'Name':'Zara','Age':7,'class':'first'}
+
+print("dict['Name']:",dict['Name'])
+print("dict['Age']:",dict['Age'])
+
+#updating dictionary
+
+dict['Age']=8;
+dict['School']="DPS School"
+
+print ("dict['Age']:",dict['Age'])
+print ("dict['School']",dict['School'])
+
+#deleting dictionary elements
+
+del dict['Name']
+
+print(dict)
+
+
---
+++ tuples.py
@@ -0,0 +1,23 @@
+#Creating a tuple
+
+tup1 = ('physics', 'chemistry',1997,2000)
+tup2 = (1,2,3,4,5,6,7)
+tup3 = "a","b","c","d"
+
+#Accessing Values of Tuples
+
+print("tup1[0]:",tup1[0])
+print("tup2[1:5]",tup2[1:5])
+
+#updating Tuples
+
+tup4=tup2+tup3
+
+print(tup4)
+
+
+#Deleting Tuple Elements
+
+del tup4;
+print("tup4 deleted")
+
