cce57f3808c6cd2bd4c22ef053103a17bbe650b2
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-24 20:34:59 +0530

    24 Jan

---
+++ practice/DataHiding.py
@@ -0,0 +1,13 @@
+#Data Hiding
+
+class JustCounter:
+    __secretCount=0
+    
+    def count(self):
+        self.__secretCount +=1
+        print(self.__secretCount)
+        
+counter=JustCounter()
+counter.count()
+counter.count()
+print(counter._JustCounter__secretCount)
